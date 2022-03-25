9adc29af21a9b40e05601ef71656e72d09747f66
Isaac Marquetti <isaac.marquetti@gmail.com>
2022-03-24 23:52:04 -0300

    Afiando o machado

--- previsao/views.py
+++ previsao/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 import requests
 
@@ -10,9 +12,13 @@ def busca(request):
     cidade = request.GET.get('cidade')
     api_key = "93f5ba284dd0564a92844a257348fa5f"
     url = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&lang=pt_br&appid={api_key}&units=metric"
+    url_city = f"http://api.openweathermap.org/geo/1.0/direct?q={cidade}&limit=5&appid={api_key}"
 
     resposta = requests.get(url).json()
+    resposta_city = requests.get(url_city).json()
 
+    nome = resposta_city[0]['name']
+    state = resposta_city[0]['state']
     temp = resposta['main']['temp']
     pais = resposta['sys']['country']
     temp_max = resposta['main']['temp_max']
@@ -23,6 +29,8 @@ def busca(request):
     foto = f'http://openweathermap.org/img/wn/{icon}@2x.png'
 
     return render(request, 'previsao/home.html', {
+        'nome': nome,
+        'estado': state,
         'local': f'{cidade}, {pais}',
         'cidade': f'{cidade}',
         'temp': f'{temp:.0f} ºC',
@@ -33,6 +41,18 @@ def busca(request):
         'icon': foto,
 
     })
+
+
+def listar_cidades(request):
+    cidade = request.GET.get('cidade')
+    api_key = "93f5ba284dd0564a92844a257348fa5f"
+
+    url_city = f"http://api.openweathermap.org/geo/1.0/direct?q={cidade}&limit=5&appid={api_key}"
+    print(url_city)
+
+    resposta_city = requests.get(url_city).json()
+    print(resposta_city)
+
 
 
 
