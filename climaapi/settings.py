a96360ea7230a47df976fbf10a6706038cfb39d3
Isaac Marquetti <isaac.marquetti@gmail.com>
2022-03-25 22:02:04 -0300

    Atualizando bugs

--- climaapi/settings.py
+++ climaapi/settings.py
@@ -172,3 +172,14 @@ if AWS_ACCESS_KEY_ID:
 
     INSTALLED_APPS.append('s3_folder_storage')
     INSTALLED_APPS.append('storages')
+
+# Mensagens
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.DEBUG: 'alert-info',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-danger',
+}
--- previsao/views.py
+++ previsao/views.py
@@ -1,7 +1,8 @@
 import json
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
+from django.contrib import messages
 
 
 def home(request):
@@ -10,6 +11,15 @@ def home(request):
 
 def busca(request):
     cidade = request.GET.get('cidade')
+
+    if cidade is None or not cidade:
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'Campo termo não pode ficar vazio'
+        )
+        return redirect('home')
+
     api_key = "93f5ba284dd0564a92844a257348fa5f"
     url = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&lang=pt_br&appid={api_key}&units=metric"
     url_city = f"http://api.openweathermap.org/geo/1.0/direct?q={cidade}&limit=5&appid={api_key}"
@@ -17,16 +27,26 @@ def busca(request):
     resposta = requests.get(url).json()
     resposta_city = requests.get(url_city).json()
 
-    nome = resposta_city[0]['name']
-    state = resposta_city[0]['state']
-    temp = resposta['main']['temp']
-    pais = resposta['sys']['country']
-    temp_max = resposta['main']['temp_max']
-    temp_min = resposta['main']['temp_min']
-    sensacao = resposta['main']['feels_like']
-    condicao = resposta['weather'][0]['description']
-    icon = resposta['weather'][0]['icon']
-    foto = f'http://openweathermap.org/img/wn/{icon}@2x.png'
+    try:
+        temp = resposta['main']['temp']
+        pais = resposta['sys']['country']
+        temp_max = resposta['main']['temp_max']
+        temp_min = resposta['main']['temp_min']
+        sensacao = resposta['main']['feels_like']
+        condicao = resposta['weather'][0]['description']
+        icon = resposta['weather'][0]['icon']
+        foto = f'http://openweathermap.org/img/wn/{icon}@2x.png'
+        nome = resposta_city[0]['name']
+    except IndexError as erro:
+        messages.add_message(request, messages.ERROR, f'Não existe a cidade "{cidade}"')
+        return redirect('home')
+
+    except KeyError as erro:
+        messages.add_message(request, messages.ERROR, f'Não encontrada a cidade "{cidade}"')
+        messages.add_message(request, messages.ERROR, f'Dica: Use acentuação')
+        return redirect('home')
+
+    state = resposta_city[0].get('state', "Sem Estado")
 
     return render(request, 'previsao/home.html', {
         'nome': nome,
