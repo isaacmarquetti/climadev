53e740c861739a306474ba39e7b65345fa60cc62
isaacmarquetti <isaac.marquetti@gmail.com>
2023-06-11 22:50:16 -0300

    Atualizando runtime

--- climaapi/settings.py
+++ climaapi/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'k=gb@6c%+(&%ia(zp-p&i^4gg7xe3r-68u_znck=-u0$ckn3qq'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
